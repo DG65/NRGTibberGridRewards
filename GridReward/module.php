215385d2a173db447ad0f924f005d51633dff6b5
DG65 <73533984+DG65@users.noreply.github.com>
2026-04-21 16:59:41 +0200

    Add files via upload

--- GridReward/module.php
+++ GridReward/module.php
@@ -5,7 +5,5 @@ class GridReward extends IPSModule
     public function Create()
     {
         parent::Create();
-        $this->RegisterPropertyString('Token', '');
-        $this->RegisterVariableBoolean('GridRewardActive','Grid Reward aktiv','~Switch');
     }
 }
