8ad944762a29ad0c10ddc78d20fe245483a0ef83
DG65 <73533984+DG65@users.noreply.github.com>
2026-04-21 18:03:27 +0200

    Add default value to 'Active' variable in GridReward
    Added a default value of 0 to the 'Active' variable.

--- GridReward/module.php
+++ GridReward/module.php
@@ -1,14 +1,16 @@
-
 <?php
+
 class GridReward extends IPSModule
 {
     public function Create()
     {
         parent::Create();
+
         $this->RegisterVariableBoolean(
-            'Active',
-            'Grid Reward aktiv',
-            '~Switch'
+            "Active",
+            "Grid Reward aktiv",
+            "~Switch",
+            0
         );
     }
 }
